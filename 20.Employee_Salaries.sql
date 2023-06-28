8a60cb63cce297107b8c3ed3243e63da56b7fd65
Rhariharan2003 <136108485+Rhariharan2003@users.noreply.github.com>
2023-06-28 13:33:53 +0530

    Create 20.Employee_Salaries.sql

---
+++ 20.Employee_Salaries.sql
@@ -0,0 +1,6 @@
+/*
+Problem Link -> https://www.hackerrank.com/challenges/salary-of-employees/problem?isFullScreen=true
+Enter your query here.
+*/
+
+select name from employee where salary > 2000 and months < 10 order by employee_id;
